c2f3314a0c5a1dc20ab6b8448eb0f2af640b28f5
Douglas Ezra Morrison <dezramorrison@gmail.com>
2022-03-07 23:46:30 -0800

    investigating assumptions

--- R/nll.R
+++ R/nll.R
@@ -1,3 +1,15 @@
+#' Title
+#'
+#' @param stratumData
+#' @param antibodies
+#' @param params
+#' @param censorLimits
+#' @param ivc If `ivc = TRUE`, the biomarker data are interval-censored.
+#' @param m
+#' @param par0
+#' @param start
+#'
+#' @return
 .nll <- function(stratumData, antibodies, params, censorLimits, ivc = FALSE, m = 0, par0, start)
 {
   # Start with zero total
@@ -34,7 +46,7 @@
       nllSingle <- .nllByType(data = data, param = param, censorLimit = censorLimit, ivc = ivc,
                               m = m, par0 = p0, start = start, modelType = modelType)
       if (!is.na(nllSingle)) {
-        nllTotal <- nllTotal + nllSingle
+        nllTotal <- nllTotal + nllSingle # DEM note: summing log likelihoods represents an independence assumption for multiple Antibodies, given time since seroconversion
       }
     }
   }

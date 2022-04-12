2449552e2b7ec1fe902f54f0d7e40004a7391c95
Douglas Ezra Morrison <dezramorrison@gmail.com>
2022-04-12 03:03:16 -0700

    more doc cleanup

--- R/nll.R
+++ R/nll.R
@@ -1,13 +1,19 @@
 #' Calculate log-likelihood
 #'
-#' @param stratumData
-#' @param antibodies
-#' @param params
-#' @param censorLimits
+#' @param stratumData Data frame with cross-sectional serology data per antibody and age, and additional
+#'   columns, for one stratum
+#' @param antibodies Character vector with one or more antibody names. Values must match \code{data}.
+#' @param params List of data frames of all longitudinal parameters. Each data frame contains
+#'   Monte Carlo samples for each antibody type.
+#' @param censorLimits List of cutoffs for one or more named antibody types (corresponding to
+#'   \code{stratumData}).
 #' @param ivc If `ivc = TRUE`, the biomarker data are interval-censored.
-#' @param m
-#' @param par0
-#' @param start
+#' @param m [not sure what this is]
+#' @param par0 List of parameters for the (lognormal) distribution of antibody concentrations
+#'   for true seronegatives (i.e. those who never seroconverted), by named antibody type
+#'   (corresponding to \code{data}).
+#' @param start  starting value for \code{log(lambda)}. Value of -6 corresponds roughly to 1 day
+#'   (log(1/365.25)), -4 corresponds roughly to 1 week (log(7 / 365.25)). Default = -6.
 #'
 #' @return
 .nll <- function(stratumData, antibodies, params, censorLimits, ivc = FALSE, m = 0, par0, start)

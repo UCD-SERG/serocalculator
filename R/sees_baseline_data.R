48ce79d5e023860c240f727023439e16dcc7d354
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-11-08 23:27:31 -0800

    dont need documentation for sees data yet

--- R/sees_baseline_data.R
+++ R/sees_baseline_data.R
@@ -1,14 +1,14 @@
-#' @docType data
-#'
-#' @name sees_crossSectional_baseline_allCountries
-#'
-#' @title
-#' Cross-sectional Typhoid data
-#'
-#' @description
-#' A [tibble::tibble()]
-#'
-#' @usage
-#' sees_crossSectional_baseline_allCountries
-#'
-NULL
+#' #' @docType data
+#' #'
+#' #' @name sees_crossSectional_baseline_allCountries
+#' #'
+#' #' @title
+#' #' Cross-sectional Typhoid data
+#' #'
+#' #' @description
+#' #' A [tibble::tibble()]
+#' #'
+#' #' @usage
+#' #' sees_crossSectional_baseline_allCountries
+#' #'
+#' NULL

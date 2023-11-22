94aeda33c82704f82007a49fb33ed6d70768b1e0
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-11-21 21:41:14 -0800

    cleanup

--- R/est.incidence.R
+++ R/est.incidence.R
@@ -3,12 +3,12 @@
 #' Age specific seroincidence function
 #' This function models seroincidence using maximum likelihood estimation; that is, it finds the value of the seroincidence parameter which maximizes the likelihood (i.e., joint probability) of the data.
 #'
-#' @param dpop cross-sectional population data
+#' @param data cross-sectional population data
 #' @param c.age age category
 #' @param start starting value for incidence rate
-#' @param antigen_isos antigen isotypes: a [character()] vector of one or more antigen isotype names, which should match the values of the `antigen_iso` column in the `dpop` input argument
+#' @param antigen_isos antigen isotypes: a [character()] vector of one or more antigen isotype names, which should match the values of the `antigen_iso` column in the `data` input argument
 #' @param noise_params a [data.frame()] containing columns `nu`, etc. specifying conditional noise parameters
-#' @param dmcmc mcmc samples from distribution of longitudinal decay curve parameters
+#' @param curve_params mcmc samples from distribution of longitudinal decay curve parameters
 #' @param verbose logical: if TRUE, print verbose log information to console
 #' @param iterlim an [integer()], which provides an upper limit on the number of computational iterations used to search for the maximum likelihood estimate of incidence (passed to [stats::nlm()]).
 #' @param stepmax a [numeric()], which limits how aggressively the [stats::nlm()] algorithm searches for the maximum likelihood estimate of incidence. If this function output an infinite standard error estimate, consider reducing this parameter.
@@ -29,11 +29,11 @@
 #' @export
 
 est.incidence <- function(
-    dpop,
-    dmcmc,
+    data,
+    curve_params,
     noise_params,
     c.age = NULL,
-    antigen_isos = dpop$antigen_iso |> unique(),
+    antigen_isos = data$antigen_iso |> unique(),
     start = 0.1,
     iterlim = 100,
     coverage = .95,
@@ -47,8 +47,8 @@ est.incidence <- function(
 
   if(!is.null(c.age))
   {
-    dpop = dpop %>% dplyr::filter(.data[["ageCat"]] == c.age)
-    dmcmc = dmcmc %>% dplyr::filter(.data[["ageCat"]] == c.age)
+    data = data %>% dplyr::filter(.data[["ageCat"]] == c.age)
+    curve_params = curve_params %>% dplyr::filter(.data[["ageCat"]] == c.age)
 
     if("ageCat" %in% names(noise_params))
     {
@@ -65,11 +65,11 @@ est.incidence <- function(
   for (cur_antigen in antigen_isos)
   {
     ps[[cur_antigen]] = get_xspd_one_antigen(
-      dpop = dpop,
+      data = data,
       antigen = cur_antigen)
 
     cs[[cur_antigen]] = get_curve_params_one_antigen(
-      params = dmcmc,
+      params = curve_params,
       antigen = cur_antigen)
 
     conds[[cur_antigen]] =

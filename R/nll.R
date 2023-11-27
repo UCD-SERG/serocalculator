86dfbd66ffad6524069c306ee9c7b17b36de5f74
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-11-26 17:20:37 -0800

    more documentation

--- R/nll.R
+++ R/nll.R
@@ -76,8 +76,8 @@ llik <- (
 
 #' Calculate negative log-likelihood
 #' @details
-#' Same as [llik()], except negated
-#' @param log.lambda log.lambda
+#' Same as [llik()], except negated and requiring lambda on log scale (used in combination with `nlm()`, to ensure that the optimization search doesn't stray into negative values of `lambda`).
+#' @param log.lambda natural logarithm of incidence rate
 #' @inheritDotParams llik -lambda
 
 #' @return the negative log-likelihood of the data with the current parameter values

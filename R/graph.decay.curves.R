48adf4e663333b47bfe44f478352137b70afd40c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-02-20 10:19:56 -0500

    one more try

--- R/graph.decay.curves.R
+++ R/graph.decay.curves.R
@@ -18,10 +18,11 @@
 #' * Setting `n_curves` larger than the number of rows in ` will result all curves being plotted.
 #' * If the user directly specifies the `rows_to_graph` argument, then `n_curves` has no effect.
 #' @examples
-#' "https://osf.io/download/rtw5k/" |>
-#' load_curve_params() |>
-#' filter(antigen_iso == "HlyE_IgG") |>
-#' plot_curve_params_one_ab()
+#' library(dplyr)
+#' "https://osf.io/download/rtw5k/" %>%
+#' load_curve_params() %>%
+#' dplyr::filter(antigen_iso == "HlyE_IgG") %>%
+#' serocalculator:::plot_curve_params_one_ab()
 plot_curve_params_one_ab = function(
     object,
     verbose = FALSE,

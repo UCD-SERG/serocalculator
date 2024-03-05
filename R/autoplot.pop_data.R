1e75192a02a2cdd730619b8b2b8287aaec82cc52
Chris Orwa <chrisorwa@gmail.com>
2024-03-05 13:53:57 +0300

    decompose function - issue 71

--- R/autoplot.pop_data.R
+++ R/autoplot.pop_data.R
@@ -22,52 +22,90 @@
 autoplot.pop_data = function(
     object,
     log =  FALSE,
+    type = 'density',
     strata = NULL,
     ...)
 {
+  if(type == 'age-scatter')
+    {
+    plot1 =
+      object %>%
+      ggplot2::ggplot(aes(x = age,y = value)) +
+      ggplot2::theme_linedraw()
 
-  plot1 =
-    object %>%
-    ggplot2::ggplot(aes(x = .data$value)) +
-    ggplot2::theme_linedraw() +
-    ggplot2::facet_wrap(~antigen_iso, nrow = 3)
+    if(is.null(strata))
+    {
+      plot1 = plot1 +
+          ggplot2::geom_point(size=.6, alpha=.7) +
+          ggplot2::geom_smooth(method=lm, se=FALSE) +
+          scale_y_log10() +
+          theme_linedraw()+
+          labs(
+              title = "Quantitative Antibody Responses by Age",
+            x = "Age",
+            y = "Value"
+            )
+    } else
+    {
+      plot1 = plot1 +
+        ggplot2::geom_point(size=.6,
+                            alpha=.7,
+                            aes(color = get(strata))) +
+        ggplot2::geom_smooth(method=lm,
+                             se=FALSE,
+                             aes(color = get(strata))
+                             ) +
+        scale_y_log10() +
+        labs(
+          title = "Quantitative Antibody Responses by Age",
+          x = "Age",
+          y = "Value",
+          colour = strata
+          )
+    }
 
-  if(!is.null(strata))
-  {
-    plot1 = plot1 +
-      ggplot2::geom_density(
-        ggplot2::aes(fill = get(strata)),
-        alpha = .6,
-        color = "black")
-  } else
-  {
-    plot1 = plot1 +
-      ggplot2::geom_density(
-        # aes(fill = get(strata)),
-        alpha = .6,
-        color = "black")
-}
+    } else
 
+    {
+    plot1 =
+        object %>%
+        ggplot2::ggplot(aes(x = value)) +
+        ggplot2::theme_linedraw() +
+        ggplot2::facet_wrap(~antigen_iso, nrow = 3)
 
-  if(log)
-  {
-    plot1 = plot1 +
-      ggplot2::scale_x_log10() +
-      ggplot2::labs(
-        title = "Distribution of Cross-sectional Antibody Responses (Log transformed)",
-        x = "Log10(Antibody Response Value)",
-        y = "Frequency"
-      )
-  } else
-  {
-    plot = plot1 +
-      ggplot2::labs(
-        title = "Distribution of Cross-sectional Antibody Responses",
-        x = "Antibody Response Value",
-        y = "Frequency"
-      )
-
-  }
+    if(is.null(strata))
+    {
+      plot1 = plot1 +
+        ggplot2::geom_density(alpha = .6,
+                              color = "black")
+    } else
+    {
+      plot1 = plot1 +
+        ggplot2::geom_density(
+          aes(fill = get(strata)),
+          alpha = .6,
+          color = "black") +
+        ggplot2::labs(fill = strata)
+    }
+    if(log)
+    {
+      plot1 = plot1 +
+        ggplot2::scale_x_log10() +
+        ggplot2::labs(
+          title = "Distribution of Cross-sectional Antibody Responses (Log transformed)",
+          x = "Log10(Antibody Response Value)",
+          y = "Frequency"
+        )
+    } else
+    {
+      plot1 = plot1 +
+        ggplot2::labs(
+          title = "Distribution of Cross-sectional Antibody Responses",
+          x = "Antibody Response Value",
+          y = "Frequency"
+        )
+    }
+    }
 
   return(plot1)
-  }
+}

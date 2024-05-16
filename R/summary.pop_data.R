fa926cbfbd9ccb8910aaa03a60c3a1de88b95c72
Chris Orwa <chrisorwa@gmail.com>
2024-05-17 01:44:31 +0300

    add group_by

--- R/summary.pop_data.R
+++ R/summary.pop_data.R
@@ -4,6 +4,7 @@
 #' This function is a `summary()` method for `pop_data` objects
 #'
 #' @param object a `pop_data` object
+#' @param strata a [character()] providing the grouping column
 #' @param ...  unused
 #'
 #' @returns a list containing two summary tables: one of `age` and one of `value`, stratified by `antigen_iso`
@@ -12,29 +13,36 @@
 #' library(dplyr)
 #'
 #' xs_data <- "https://osf.io/download//n6cp3/" %>%
-#' load_pop_data() %>%
-#' clean_pop_data()
+#'   load_pop_data() %>%
+#'   clean_pop_data()
 #'
 #' xs_data %>% summary()
 #'
-summary.pop_data = function(object, ...)
-{
-
-  ages =
+summary.pop_data <- function(object, strata = "Country", ...) {
+  ages <-
     object %>%
-    distinct(.data$id, .data$age)
+    distinct(.data$id, .data$age, .data[[strata]])
 
-  cat("\nn =", nrow(ages),"\n")
+  cat("\nn =", nrow(ages), "\n")
 
   cat("\nDistribution of age: \n\n")
-  age_summary =
-    ages$age %>%
-    summary() %>%
+  age_summary <-
+    ages %>%
+    select(age, all_of(strata)) %>%
+    group_by(across(all_of(strata))) %>%
+    summarise(
+      age_min = min(age),
+      age_first_quartile = quantile(age, 0.25),
+      age_median = median(age),
+      age_mean = mean(age),
+      age_third_quartile = quantile(age, 0.75),
+      age_max = max(age)
+    ) %>%
     print()
 
-  cat('\nDistributions of antigen-isotype measurements:\n\n')
+  cat("\nDistributions of antigen-isotype measurements:\n\n")
 
-  ab_summary =
+  ab_summary <-
     object %>%
     dplyr::summarize(
       .by = .data$antigen_iso,
@@ -48,10 +56,11 @@ summary.pop_data = function(object, ...)
     as.data.frame() %>%
     print()
 
-  to_return = list(
+  to_return <- list(
     n = nrow(ages),
     age_summary = age_summary,
-    ab_summary = ab_summary)
+    ab_summary = ab_summary
+  )
 
   return(invisible(to_return))
 }

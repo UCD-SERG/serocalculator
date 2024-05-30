3daf5767e28cb2c491b8a17067bc0d2ea44e6649
Chris Orwa <chrisorwa@gmail.com>
2024-05-30 23:01:50 +0300

    change age column referencing'

--- R/summary.pop_data.R
+++ R/summary.pop_data.R
@@ -13,28 +13,39 @@
 #' library(dplyr)
 #'
 #' xs_data <- load_pop_data("https://osf.io/download//n6cp3/")
+#' summary(xs_data, strata = "Country")
 #'
-#' summary(xs_data)
-#'
-summary.pop_data <- function(object, ...) {
+summary.pop_data <- function(object, strata = "Country", ...) {
+  # get relevant columns from object
+  age_column <- object %>% get_age_var()
+  value_column <- object %>% get_value_var()
+  id_column <- object %>% get_id_var()
+
   ages <-
     object %>%
-    distinct(.data$id, .data$age, .data[[strata]])
+    distinct(
+      .data[[age_column]],
+      .data[[id_column]],
+      .data[[strata]]
+    )
 
   cat("\nn =", nrow(ages), "\n")
 
   cat("\nDistribution of age: \n\n")
   age_summary <-
     ages %>%
-    select(age, all_of(strata)) %>%
+    select(
+      age_column,
+      all_of(strata)
+    ) %>%
     group_by(across(all_of(strata))) %>%
     summarise(
-      age_min = min(age),
-      age_first_quartile = quantile(age, 0.25),
-      age_median = median(age),
-      age_mean = mean(age),
-      age_third_quartile = quantile(age, 0.75),
-      age_max = max(age)
+      age_min = min(.data[[age_column]]),
+      age_first_quartile = quantile(.data[[age_column]], 0.25),
+      age_median = median(.data[[age_column]]),
+      age_mean = mean(.data[[age_column]]),
+      age_third_quartile = quantile(.data[[age_column]], 0.75),
+      age_max = max(.data[[age_column]])
     ) %>%
     print()
 
@@ -44,12 +55,25 @@ summary.pop_data <- function(object, ...) {
     object %>%
     dplyr::summarize(
       .by = .data$antigen_iso,
-      Min = object %>% get_value() %>% min(na.rm = TRUE),
-      `1st Qu.` = object %>% get_value() %>% quantile(.25, na.rm = TRUE),
-      Median = object %>% get_value() %>% median(),
-      `3rd Qu.` = object %>% get_value() %>% quantile(.75, na.rm = TRUE),
-      Max = object %>% get_value() %>% max(na.rm = TRUE),
-      `# NAs` = object %>% get_value() %>% is.na() %>% sum()
+      Min = object %>%
+        get_value() %>%
+        min(na.rm = TRUE),
+      `1st Qu.` = object %>%
+        get_value() %>%
+        quantile(.25, na.rm = TRUE),
+      Median = object %>%
+        get_value() %>%
+        median(),
+      `3rd Qu.` = object %>%
+        get_value() %>%
+        quantile(.75, na.rm = TRUE),
+      Max = object %>%
+        get_value() %>%
+        max(na.rm = TRUE),
+      `# NAs` = object %>%
+        get_value() %>%
+        is.na() %>%
+        sum()
     ) %>%
     as.data.frame() %>%
     print()

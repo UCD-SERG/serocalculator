11b217b57d04def223a01208630702d7baee0856
Chris Orwa <chrisorwa@gmail.com>
2024-05-31 08:34:43 +0300

    set strata to NULL

--- R/summary.pop_data.R
+++ R/summary.pop_data.R
@@ -15,74 +15,128 @@
 #' xs_data <- load_pop_data("https://osf.io/download//n6cp3/")
 #' summary(xs_data, strata = "Country")
 #'
-summary.pop_data <- function(object, strata = "Country", ...) {
+summary.pop_data <- function(object, strata = NULL, ...) {
   # get relevant columns from object
   age_column <- object %>% get_age_var()
   value_column <- object %>% get_value_var()
   id_column <- object %>% get_id_var()
 
-  ages <-
-    object %>%
-    distinct(
-      .data[[age_column]],
-      .data[[id_column]],
-      .data[[strata]]
+  if (is.null(strata)) {
+    ages <-
+      object %>%
+      distinct(
+        .data[[age_column]],
+        .data[[id_column]]
+      )
+
+    age_summary <-
+      ages %>%
+      pull(age_column) %>%
+      summary() %>%
+      print()
+
+    cat("\nn =", nrow(ages), "\n")
+
+    cat("\nDistribution of age: \n\n")
+
+    ab_summary <-
+      object %>%
+      dplyr::summarize(
+        .by = .data$antigen_iso,
+        Min = object %>%
+          get_value() %>%
+          min(na.rm = TRUE),
+        `1st Qu.` = object %>%
+          get_value() %>%
+          quantile(.25, na.rm = TRUE),
+        Median = object %>%
+          get_value() %>%
+          median(),
+        `3rd Qu.` = object %>%
+          get_value() %>%
+          quantile(.75, na.rm = TRUE),
+        Max = object %>%
+          get_value() %>%
+          max(na.rm = TRUE),
+        `# NAs` = object %>%
+          get_value() %>%
+          is.na() %>%
+          sum()
+      ) %>%
+      as.data.frame() %>%
+      print()
+
+    to_return <- list(
+      n = nrow(ages),
+      age_summary = age_summary,
+      ab_summary = ab_summary
     )
 
-  cat("\nn =", nrow(ages), "\n")
+    return(invisible(to_return))
+  } else {
+    ages <-
+      object %>%
+      distinct(
+        .data[[age_column]],
+        .data[[id_column]],
+        .data[[strata]]
+      )
 
-  cat("\nDistribution of age: \n\n")
-  age_summary <-
-    ages %>%
-    select(
-      age_column,
-      all_of(strata)
-    ) %>%
-    group_by(across(all_of(strata))) %>%
-    summarise(
-      age_min = min(.data[[age_column]]),
-      age_first_quartile = quantile(.data[[age_column]], 0.25),
-      age_median = median(.data[[age_column]]),
-      age_mean = mean(.data[[age_column]]),
-      age_third_quartile = quantile(.data[[age_column]], 0.75),
-      age_max = max(.data[[age_column]])
-    ) %>%
-    print()
+    cat("\nn =", nrow(ages), "\n")
 
-  cat("\nDistributions of antigen-isotype measurements:\n\n")
+    cat("\nDistribution of age: \n\n")
+    age_summary <-
+      ages %>%
+      select(
+        all_of(age_column),
+        all_of(strata)
+      ) %>%
+      summarise(
+        age_min = min(.data[[age_column]]),
+        age_first_quartile = quantile(.data[[age_column]], 0.25),
+        age_median = median(.data[[age_column]]),
+        age_mean = mean(.data[[age_column]]),
+        age_third_quartile = quantile(.data[[age_column]], 0.75),
+        age_max = max(.data[[age_column]]),
+        .by = strata
+      ) %>%
+      print()
 
-  ab_summary <-
-    object %>%
-    dplyr::summarize(
-      .by = .data$antigen_iso,
-      Min = object %>%
-        get_value() %>%
-        min(na.rm = TRUE),
-      `1st Qu.` = object %>%
-        get_value() %>%
-        quantile(.25, na.rm = TRUE),
-      Median = object %>%
-        get_value() %>%
-        median(),
-      `3rd Qu.` = object %>%
-        get_value() %>%
-        quantile(.75, na.rm = TRUE),
-      Max = object %>%
-        get_value() %>%
-        max(na.rm = TRUE),
-      `# NAs` = object %>%
-        get_value() %>%
-        is.na() %>%
-        sum()
-    ) %>%
-    as.data.frame() %>%
-    print()
+    cat("\nDistributions of antigen-isotype measurements:\n\n")
 
-  to_return <- list(
-    n = nrow(ages),
-    age_summary = age_summary,
-    ab_summary = ab_summary
-  )
+    ab_summary <-
+      object %>%
+      dplyr::summarize(
+        .by = .data$antigen_iso,
+        Min = object %>%
+          get_value() %>%
+          min(na.rm = TRUE),
+        `1st Qu.` = object %>%
+          get_value() %>%
+          quantile(.25, na.rm = TRUE),
+        Median = object %>%
+          get_value() %>%
+          median(),
+        `3rd Qu.` = object %>%
+          get_value() %>%
+          quantile(.75, na.rm = TRUE),
+        Max = object %>%
+          get_value() %>%
+          max(na.rm = TRUE),
+        `# NAs` = object %>%
+          get_value() %>%
+          is.na() %>%
+          sum()
+      ) %>%
+      as.data.frame() %>%
+      print()
 
-  return(invisible(to_return))
+    to_return <- list(
+      n = nrow(ages),
+      age_summary = age_summary,
+      ab_summary = ab_summary
+    )
+
+    return(invisible(to_return))
+  }
 }

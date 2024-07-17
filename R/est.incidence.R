e25e02c032e5b6b5fff0a2c9a35446fb1e042d2e
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-07-17 14:31:47 -0400

    keep subclass

--- R/est.incidence.R
+++ R/est.incidence.R
@@ -64,7 +64,7 @@ est.incidence <- function(
       pop_data %>% get_age_var(),
       "antigen_iso"
     ) %>%
-    tidyr::drop_na()
+    filter(across(everything(), ~ !is.na(.x)))
 
   curve_params <- curve_params %>%
     ungroup() %>%

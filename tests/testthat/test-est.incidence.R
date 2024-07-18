7bb2759ceccc0fa81b0de219c1a018dd270b4c6a
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-07-18 11:24:12 -0400

    formatting

--- tests/testthat/test-est.incidence.R
+++ tests/testthat/test-est.incidence.R
@@ -48,46 +48,46 @@ test_that(
   })
 
 test_that("`est.incidence()` produces the same results regardless of name standardization", {
-            # get noise data
-            noise <- load_noise_params("https://osf.io/download//hqy4v/") %>%
-              filter(Country == "Pakistan")
+  # get noise data
+  noise <- load_noise_params("https://osf.io/download//hqy4v/") %>%
+    filter(Country == "Pakistan")
 
-            # get curve data
-            curves <- load_curve_params("https://osf.io/download/rtw5k/") %>%
-              slice_head(n = 100, by = "antigen_iso")
-            xs_data_true <- load_pop_data(
-              file_path = "https://osf.io/download//n6cp3/",
-              age = "Age",
-              value = "result",
-              id = "index_id",
-              standardize = TRUE
-            )  %>%
-              filter(Country == "Pakistan") %>%
-              slice_head(n = 100, by = "antigen_iso")
+  # get curve data
+  curves <- load_curve_params("https://osf.io/download/rtw5k/") %>%
+    slice_head(n = 100, by = "antigen_iso")
+  xs_data_true <- load_pop_data(
+    file_path = "https://osf.io/download//n6cp3/",
+    age = "Age",
+    value = "result",
+    id = "index_id",
+    standardize = TRUE
+  )  %>%
+    filter(Country == "Pakistan") %>%
+    slice_head(n = 100, by = "antigen_iso")
 
-            est_true <- est.incidence(
-              pop_data = xs_data_true,
-              curve_params = curves,
-              noise_params = noise,
-              antigen_isos = c("HlyE_IgG", "HlyE_IgA")
-            )
+  est_true <- est.incidence(
+    pop_data = xs_data_true,
+    curve_params = curves,
+    noise_params = noise,
+    antigen_isos = c("HlyE_IgG", "HlyE_IgA")
+  )
 
-            xs_data_false <- load_pop_data(
-              file_path = "https://osf.io/download//n6cp3/",
-              age = "Age",
-              value = "result",
-              id = "index_id",
-              standardize = FALSE
-            )  %>%
-              filter(Country == "Pakistan") %>%
-              slice_head(n = 100, by = "antigen_iso")
+  xs_data_false <- load_pop_data(
+    file_path = "https://osf.io/download//n6cp3/",
+    age = "Age",
+    value = "result",
+    id = "index_id",
+    standardize = FALSE
+  )  %>%
+    filter(Country == "Pakistan") %>%
+    slice_head(n = 100, by = "antigen_iso")
 
-            est_false <- est.incidence(
-              pop_data = xs_data_false %>% filter(Country == "Pakistan"),
-              curve_params = curves,
-              noise_params = noise %>% filter(Country == "Pakistan"),
-              antigen_isos = c("HlyE_IgG", "HlyE_IgA")
-            )
+  est_false <- est.incidence(
+    pop_data = xs_data_false %>% filter(Country == "Pakistan"),
+    curve_params = curves,
+    noise_params = noise %>% filter(Country == "Pakistan"),
+    antigen_isos = c("HlyE_IgG", "HlyE_IgA")
+  )
 
-            expect_equal(est_true, est_false)
-          })
+  expect_equal(est_true, est_false)
+})

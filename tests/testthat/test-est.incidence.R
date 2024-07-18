036245873924a39aedf40e3f33d0e338ea203e5f
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-07-18 11:21:50 -0400

    attempting to reconcile

--- tests/testthat/test-est.incidence.R
+++ tests/testthat/test-est.incidence.R
@@ -1,49 +1,51 @@
-test_that("est.incidence() produces expected results for typhoid data", {
-  library(dplyr)
-  # get pop data
-  xs_data <- load_pop_data(
-    file_path = "https://osf.io/download//n6cp3/",
-    age = "Age",
-    value = "result",
-    id = "index_id",
-    standardize = TRUE
-  ) %>%
-    filter(Country == "Pakistan") %>%
-    slice_head(n = 100, by = "antigen_iso")
+test_that(
+  "est.incidence() produces expected results for typhoid data",
+  {
+    library(dplyr)
+    # get pop data
+    xs_data <- load_pop_data(
+      file_path = "https://osf.io/download//n6cp3/",
+      age = "Age",
+      value = "result",
+      id = "index_id",
+      standardize = TRUE
+    ) %>%
+      filter(Country == "Pakistan") %>%
+      slice_head(n = 100, by = "antigen_iso")
 
-  # get noise data
-  noise <- load_noise_params("https://osf.io/download//hqy4v/") %>%
-    filter(Country == "Pakistan")
+    # get noise data
+    noise <- load_noise_params("https://osf.io/download//hqy4v/") %>%
+      filter(Country == "Pakistan")
 
-  # get curve data
-  curve <- load_curve_params("https://osf.io/download/rtw5k/") %>%
-    slice_head(n = 100, by = "antigen_iso")
+    # get curve data
+    curve <- load_curve_params("https://osf.io/download/rtw5k/") %>%
+      slice_head(n = 100, by = "antigen_iso")
 
-  # set start
-  start <- .05
+    # set start
+    start <- .05
 
-  typhoid_results <- est.incidence(
-    pop_data = xs_data,
-    curve_param = curve,
-    noise_param = noise,
-    antigen_isos = c("HlyE_IgG", "HlyE_IgA")
-  )
+    typhoid_results <- est.incidence(
+      pop_data = xs_data,
+      curve_param = curve,
+      noise_param = noise,
+      antigen_isos = c("HlyE_IgG", "HlyE_IgA")
+    )
 
-  expect_snapshot(x = typhoid_results)
+    expect_snapshot(x = typhoid_results)
 
-  expect_snapshot_value(x = typhoid_results,
-                        style = "deparse",
-                        tolerance = 10 ^ -5)
+    expect_snapshot_value(x = typhoid_results,
+                          style = "deparse",
+                          tolerance = 10 ^ -5)
 
-  typhoid_results_summary =
-    summary(typhoid_results, coverage = .95)
+    typhoid_results_summary =
+      summary(typhoid_results, coverage = .95)
 
-  expect_snapshot(x = typhoid_results_summary)
+    expect_snapshot(x = typhoid_results_summary)
 
-  expect_snapshot_value(x = typhoid_results_summary,
-                        style = "deparse",
-                        tolerance = 10 ^ -5)
-})
+    expect_snapshot_value(x = typhoid_results_summary,
+                          style = "deparse",
+                          tolerance = 10 ^ -5)
+  })
 
 test_that("`est.incidence()` produces the same results regardless of name standardization",
           {

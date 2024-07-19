0eec796f17a5f81d8bbe81900e35d700ec3b88fb
Chris Orwa <chrisorwa@gmail.com>
2024-07-19 21:44:43 +0300

    make corrections to tests

--- tests/testthat/test-as_curve_params.R
+++ tests/testthat/test-as_curve_params.R
@@ -5,16 +5,18 @@ test_that("`as_curve_params()` produces an error when non-curve data is provided
       "https://osf.io/download//n6cp3/" %>% # pop data
       readr::read_rds() %>%
       as_curve_params(),
-    regexp = "Please provide curve data",
-    fixed = TRUE
+    class = "not curve_params"
   )
 })
 
+
 test_that("`as_curve_params()` produces expected results", {
-  expect_snapshot(
-    curve_data <-
-      "https://osf.io/download/rtw5k/" %>% # curve data
-      readr::read_rds() %>%
-      as_curve_params()
+  test_data <- "https://osf.io/download/rtw5k/" %>% # curve data
+    readr::read_rds() %>%
+    as_curve_params()
+
+  expect_snapshot_value(
+    x = test_data,
+    style = "serialize"
   )
 })

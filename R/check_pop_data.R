33b0ec7320cfe5fd7f7dd70fc7440b3ebcc33c1b
Chris Orwa <chrisorwa@gmail.com>
2024-09-04 22:08:55 +0300

    lint file

--- R/check_pop_data.R
+++ R/check_pop_data.R
@@ -16,7 +16,8 @@ check_pop_data <- function(pop_data, verbose = FALSE) {
       class = "not a data.frame",
       message = c(
         "Argument `pop_data` is not a `data.frame()`.",
-        "Provide a `data.frame()` with cross-sectional serology data per antigen isotype."
+        "Provide a `data.frame()` with cross-sectional serology
+        data per antigen isotype."
       )
     )
   }
@@ -27,7 +28,8 @@ check_pop_data <- function(pop_data, verbose = FALSE) {
   )
 
   if (!missing_age) {
-    "Argument {.arg pop_data} is missing column {.var {pop_data %>% get_age_var()}} (age, in years)" %>%
+    "Argument {.arg pop_data} is missing column
+    {.var {pop_data %>% get_age_var()}}(age, in years)" %>%
       cli::cli_abort(class = "missing-var")
   }
 
@@ -37,7 +39,8 @@ check_pop_data <- function(pop_data, verbose = FALSE) {
   )
 
   if (!missing_value) {
-    "Argument {.arg pop_data} is missing column {.var {pop_data %>% get_value_var()}} (antibody measurement)" %>%
+    "Argument {.arg pop_data} is missing column
+    {.var {pop_data %>% get_value_var()}} (antibody measurement)" %>%
       cli::cli_abort(class = "missing-var")
   }
 

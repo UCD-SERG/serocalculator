b99295bf8f37876c1537c0944fd5c304a6fdaf90
Chris Orwa <chrisorwa@gmail.com>
2024-09-07 16:00:36 +0300

    linting

--- R/load_noise_params.R
+++ R/load_noise_params.R
@@ -1,9 +1,14 @@
 #' Load noise parameters
 #'
-#' @param file_path path to an RDS file containing biologic and measurement noise of antibody decay curve parameters `y.low`, `eps`, `nu`, and `y.high`, stored as a [data.frame()] or [tibble::tbl_df]
-#' @param antigen_isos [character()] vector of antigen isotypes to be used in analyses
+#' @param file_path path to an RDS file containing biologic
+#' and measurement noise of antibody decay curve parameters
+#' `y.low`, `eps`, `nu`, and `y.high`,
+#' stored as a [data.frame()] or [tibble::tbl_df]
+#' @param antigen_isos [character()] vector
+#' of antigen isotypes to be used in analyses
 #'
-#' @returns a `noise` object (a [tibble::tbl_df] with extra attribute `antigen_isos`)
+#' @returns a `noise` object (a [tibble::tbl_df]
+#' with extra attribute `antigen_isos`)
 #' @export
 #' @examples
 #' noise <- load_noise_params("https://osf.io/download//hqy4v/")

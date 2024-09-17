f4c3b7d27ebb127bc421d2c6ca6a224a479566b8
Chris Orwa <chrisorwa@gmail.com>
2024-09-17 11:19:03 +0300

    lint test file

--- tests/testthat/test-autoplot.pop_data.R
+++ tests/testthat/test-autoplot.pop_data.R
@@ -1,15 +1,14 @@
 test_that("`autoplot.pop_data()` raise
           an error when unavailale type is provided", {
-  xs_data <- load_pop_data(
-    file_path = "https://osf.io/download//n6cp3/",
-    age = "Age",
-    id = "index_id",
-    value = "result",
-    standardize = TRUE
-  )
-
-  expect_error(
-    object = xs_data %>%
-      autoplot(strata = "Country", type = "den")
-  )
-})
+            xs_data <- load_pop_data(
+              file_path = "https://osf.io/download//n6cp3/",
+              age = "Age",
+              id = "index_id",
+              value = "result",
+              standardize = TRUE
+            )
+            expect_error(
+              object = xs_data %>%
+                autoplot(strata = "Country", type = "den")
+            )
+          })

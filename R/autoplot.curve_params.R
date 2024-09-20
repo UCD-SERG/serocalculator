3a6f12f258975c611e5c21a4c49fc09bc668a034
kristinawlai <kwlai@ucdavis.edu>
2024-09-19 20:02:57 -0700

    Fixing line lengths and open brackets noted during linting

--- R/autoplot.curve_params.R
+++ R/autoplot.curve_params.R
@@ -2,11 +2,19 @@
 #'
 #' @inheritParams plot_curve_params_one_ab
 #' @inheritDotParams plot_curve_params_one_ab
-#' @param antigen_isos antigen isotypes to analyze (can be used to subset `curve_params`)
+#' @param antigen_isos antigen isotypes to analyze (can subset `curve_params`)
 #' @param ncol how many columns of subfigures to use in panel plot
 #' @details
 #' ## `rows_to_graph`
-#' Note that if you directly specify `rows_to_graph` when calling this function, the row numbers are enumerated separately for each antigen isotype; in other words, for the purposes of this argument, row numbers start over at 1 for each antigen isotype. There is currently no way to specify different row numbers for different antigen isotypes; if you want to do that, you will could call [plot_curve_params_one_ab()] directly for each antigen isotype and combine the resulting panels yourself. Or you could subset `curve_params` manually, before passing it to this function, and set the `n_curves` argument to `Inf`.
+#' Note that if you directly specify `rows_to_graph` when calling this function,
+#' the row numbers are enumerated separately for each antigen isotype;
+#' in other words, for the purposes of this argument,
+#' row numbers start over at 1 for each antigen isotype. There is currently
+#' no way to specify different row numbers for different antigen isotypes;
+#' if you want to do that, you could call [plot_curve_params_one_ab()]
+#' directly for each antigen isotype and combine the resulting panels yourself.
+#' Or you could subset `curve_params` manually, before passing it to this
+#' function, and set the `n_curves` argument to `Inf`.
 #' @return a [ggplot2::ggplot()] object
 #' @export
 #' @examples
@@ -17,7 +25,7 @@
 #'
 #' curve = load_curve_params("https://osf.io/download/rtw5k/") %>%
 #'   filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG")) %>%
-#'   slice(1:100, .by = antigen_iso)  %>% # Reduce dataset for the purposes of this example
+#'   slice(1:100, .by = antigen_iso)  %>% # Reduce dataset for this example
 #'   autoplot()
 #'
 #' curve
@@ -36,8 +44,7 @@ autoplot.curve_params <- function(
   figs <- split_data %>%
     lapply(FUN = plot_curve_params_one_ab, ...)
 
-  for (i in seq_along(figs))
-  {
+  for (i in seq_along(figs)) {
     figs[[i]] <- figs[[i]] + ggplot2::ggtitle(labels[i])
   }
 

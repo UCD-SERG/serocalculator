7a60546f6ca9538af6def8bd1b30234339eb74e1
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-09-24 22:24:06 -0700

    lint

--- R/df_to_array.R
+++ R/df_to_array.R
@@ -18,10 +18,14 @@ df.to.array <- function(
 
 #' Convert a data.frame (or tibble) into a multidimensional array
 #'
-#' @param df a [data.frame()] (or [tibble::tibble()]) in long format (each row contains one value for the intended array)
-#' @param dim_var_names a [character()] vector of variable names in `df`. All of these variables should be factors, or a warning will be produced.
-#' @param value_var_name a [character()] variable containing a variable name from `df` which contains the values for the intended array.
-#' @return an [array()] with dimensions defined by the variables in `df` listed in `dim_var_names`
+#' @param df a [data.frame()] (or [tibble::tibble()]) in long format
+#' (each row contains one value for the intended array)
+#' @param dim_var_names a [character()] vector of variable names in `df`.
+#' All of these variables should be factors, or a warning will be produced.
+#' @param value_var_name a [character()] variable containing a variable name
+#' from `df` which contains the values for the intended array.
+#' @return an [array()] with dimensions defined by the variables in `df`
+#' listed in `dim_var_names`
 #'
 #' @examples
 #' library(dplyr)
@@ -33,7 +37,9 @@ df.to.array <- function(
 #'     cols = c("Sepal.Length", "Sepal.Width", "Petal.Width", "Petal.Length")
 #'   ) %>%
 #'   mutate(parameter = factor(parameter, levels = unique(parameter)))
-#' arr <- df %>% serocalculator:::df_to_array(dim_var_names = c("parameter", "Species"))
+#' arr <- df %>%
+#'   serocalculator:::df_to_array(
+#'      dim_var_names = c("parameter", "Species"))
 #' ftable(arr[,,1:5])
 #' @noRd
 df_to_array <- function(
@@ -77,6 +83,6 @@ df_to_array <- function(
     )
 
   df %>%
-    mutate(.by = all_of(dim_var_names), obs = 1:n()) %>%
+    mutate(.by = all_of(dim_var_names), obs = row_number()) %>%
     xtabs(formula = formula(xtabs_formula))
 }

9986adeb55457c69365210945f3d2ade9aca90f5
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-09-25 20:35:39 -0700

    remove duplicate test

--- tests/testthat/test-class_attributes.R
+++ tests/testthat/test-class_attributes.R
@@ -8,14 +8,6 @@ test_that("`get_biomarker_levels()` works", {
   expect_equal(object = biomarker_levels, expected = expected_levels)
 })
 
-
-test_that("`set_biomarker_var()` halts on misspecified column", {
-  xs_data <- "https://osf.io/download//n6cp3/" %>%
-    readr::read_rds() %>%
-    set_biomarker_var("biomarker") %>%
-    expect_error(class = "missing variable")
-})
-
 test_that("`get_id()` works", {
   xs_data <- "https://osf.io/download//n6cp3/" %>%
     load_pop_data()

e1233c34d5b7d87c4a988944367d47edea54567c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-09-25 18:24:30 -0700

    in progress

--- R/graph.loglik.R
+++ R/graph.loglik.R
@@ -47,7 +47,7 @@ graph.loglik = function(
     pop_data,
     curve_params,
     noise_params,
-    antigen_isos,
+    antigen_isos = pop_data %>% get_biomarker_levels(),
     x = 10^seq(-3, 0, by = .1),
     highlight_points = NULL,
     highlight_point_names = "highlight_points",

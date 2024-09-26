120acee2f2dc5c307b87d30382fdcc64c3f72ec2
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-09-25 20:39:10 -0700

    more lint

--- tests/testthat/test-class_attributes.R
+++ tests/testthat/test-class_attributes.R
@@ -1,8 +1,8 @@
 test_that("`get_biomarker_levels()` works", {
   xs_data <- "https://osf.io/download//n6cp3/" %>%
     load_pop_data()
-  biomarker_levels = xs_data %>% get_biomarker_levels()
-  expected_levels = structure(1:2,
+  biomarker_levels <- xs_data %>% get_biomarker_levels()
+  expected_levels <- structure(1:2,
                               levels = c("HlyE_IgA", "HlyE_IgG"),
                               class = "factor")
   expect_equal(object = biomarker_levels, expected = expected_levels)

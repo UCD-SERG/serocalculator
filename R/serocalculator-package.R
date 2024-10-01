bb4086d47d33868a3b2e0094ef8e1e4b917e931d
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-01 11:06:03 -0700

    lint

--- R/serocalculator-package.R
+++ R/serocalculator-package.R
@@ -22,7 +22,7 @@
 #' [load_curve_params()]                  | loading antibody decay curve models
 #' [autoplot.curve_params()]              | graphing antibody decay curves
 #' [llik()]                               | computing log-likelihoods
-#' [graph.loglik()]                       | graphing log-likelihood functions
+#' [graph_loglik()]                       | graphing log-likelihood functions
 #' [autoplot.seroincidence()]             | graphing log-likelihood functions
 #' [autoplot.seroincidence.by()]          | graphing log-likelihood functions
 #' [est.incidence()]                      | estimating incidence rates

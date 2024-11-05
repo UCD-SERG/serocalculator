d94b6bdc53934dd0054645142c65e652d762b979
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-04 21:51:09 -0800

    - added test data

---
+++ data-raw/sees_pop_data_pakistan_100.R
@@ -0,0 +1,23 @@
+sees_pop_data_pk_100_standardized <- load_pop_data(
+  file_path = "https://osf.io/download//n6cp3/",
+  age = "Age",
+  value = "result",
+  id = "index_id",
+  standardize = TRUE
+) %>%
+  filter(Country == "Pakistan") %>%
+  slice_head(n = 100, by = antigen_iso)
+
+usethis::use_data(sees_pop_data_pk_100_standardized, overwrite = TRUE)
+
+sees_pop_data_pk_100_nonstandardized <- load_pop_data(
+  file_path = "https://osf.io/download//n6cp3/",
+  age = "Age",
+  value = "result",
+  id = "index_id",
+  standardize = FALSE
+) %>%
+  filter(Country == "Pakistan") %>%
+  slice_head(n = 100, by = antigen_iso)
+
+usethis::use_data(sees_pop_data_pk_100_nonstandardized, overwrite = TRUE)

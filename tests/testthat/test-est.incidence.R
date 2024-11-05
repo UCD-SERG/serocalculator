da45b475c117e34169b5f0485d959b8c58ec38c7
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-04 22:37:05 -0800

    set tolerance to allow for differences on mac

--- tests/testthat/test-est.incidence.R
+++ tests/testthat/test-est.incidence.R
@@ -1,37 +1,34 @@
-test_that(
-  "est.incidence() produces expected results for typhoid data",
-  {
-
-    typhoid_results <- est.incidence(
-      pop_data = sees_pop_data_pk_100_standardized,
-      curve_param = typhoid_curves_nostrat_100,
-      noise_param = example_noise_params_pk,
-      antigen_isos = c("HlyE_IgG", "HlyE_IgA")
-    )
-
-    expect_snapshot(x = summary(typhoid_results))
-
-    expect_snapshot_value(
-      typhoid_results, style = "deparse"
-    )
-  }
-)
-
-test_that("`est.incidence()` produces expected results", {
-
-  est_true <- est.incidence(
+test_that("est.incidence() produces expected results for typhoid data", {
+  typhoid_results <- est.incidence(
     pop_data = sees_pop_data_pk_100_standardized,
     curve_param = typhoid_curves_nostrat_100,
     noise_param = example_noise_params_pk,
     antigen_isos = c("HlyE_IgG", "HlyE_IgA")
   )
 
-  est_false <- est.incidence(
-    pop_data = sees_pop_data_pk_100_nonstandardized,
-    curve_param = typhoid_curves_nostrat_100,
-    noise_param = example_noise_params_pk,
-    antigen_isos = c("HlyE_IgG", "HlyE_IgA")
-  )
+  expect_snapshot(x = summary(typhoid_results))
 
-  expect_equal(est_true, est_false)
+  expect_snapshot_value(typhoid_results, style = "deparse", tolerance = 1e-4)
 })
+
+test_that(
+  "`est.incidence()` produces consistent results
+          regardless of whether data colnames are standardized.",
+  {
+    est_true <- est.incidence(
+      pop_data = sees_pop_data_pk_100_standardized,
+      curve_param = typhoid_curves_nostrat_100,
+      noise_param = example_noise_params_pk,
+      antigen_isos = c("HlyE_IgG", "HlyE_IgA")
+    )
+
+    est_false <- est.incidence(
+      pop_data = sees_pop_data_pk_100_nonstandardized,
+      curve_param = typhoid_curves_nostrat_100,
+      noise_param = example_noise_params_pk,
+      antigen_isos = c("HlyE_IgG", "HlyE_IgA")
+    )
+
+    expect_equal(est_true, est_false)
+  }
+)

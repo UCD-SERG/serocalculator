97f8f35175e7144cf0a30dcbdd03efdc8bd99d7b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-04 23:11:06 -0800

    update names

--- tests/testthat/test-est.incidence.R
+++ tests/testthat/test-est.incidence.R
@@ -1,6 +1,6 @@
 test_that("est.incidence() produces expected results for typhoid data", {
   typhoid_results <- est.incidence(
-    pop_data = sees_pop_data_pk_100_standardized,
+    pop_data = sees_pop_data_pk_100,
     curve_param = typhoid_curves_nostrat_100,
     noise_param = example_noise_params_pk,
     antigen_isos = c("HlyE_IgG", "HlyE_IgA")
@@ -16,14 +16,14 @@ test_that(
           regardless of whether data colnames are standardized.",
   {
     est_true <- est.incidence(
-      pop_data = sees_pop_data_pk_100_standardized,
+      pop_data = sees_pop_data_pk_100,
       curve_param = typhoid_curves_nostrat_100,
       noise_param = example_noise_params_pk,
       antigen_isos = c("HlyE_IgG", "HlyE_IgA")
     )
 
     est_false <- est.incidence(
-      pop_data = sees_pop_data_pk_100_nonstandardized,
+      pop_data = sees_pop_data_pk_100_old_names,
       curve_param = typhoid_curves_nostrat_100,
       noise_param = example_noise_params_pk,
       antigen_isos = c("HlyE_IgG", "HlyE_IgA")

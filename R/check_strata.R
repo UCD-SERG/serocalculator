543788fc460e9b2e8f585121fc196460a7f48cdc
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-05 12:46:35 -0800

    lint

--- R/check_strata.R
+++ R/check_strata.R
@@ -4,39 +4,46 @@
 #' @returns [NULL], invisibly
 #' @examples
 #' sees_pop_data_pk_100 |>
-#'    check_strata(strata = c("ag", "catch","Count"))
+#'   check_strata(strata = c("ag", "catch", "Count"))
 #' @dev
 check_strata <- function(pop_data, strata) {
   if (!is.character(strata)) {
     cli::cli_abort(
       class = "strata are not strings",
-      c("x" = "Argument `strata` is not a character vector.", "i" = "Provide a character vector with names of stratifying variables.")
+      message = c(
+        "x" = "Argument `strata` is not a character vector.",
+        "i" = "Provide a character vector with names of stratifying variables."
+      )
     )
   }
 
-  present_strata_vars = intersect(strata, names(pop_data))
-  missing_strata_vars = setdiff(strata, present_strata_vars)
+  present_strata_vars <- intersect(strata, names(pop_data))
+  missing_strata_vars <- setdiff(strata, present_strata_vars)
 
   if (length(missing_strata_vars) > 0) {
-    message0 = c("Can't stratify provided {.arg pop_data} with the provided {.arg strata}:",
-                 "i" = "variable {.var {missing_strata_vars}} {?is/are} missing in {.arg pop_data}.")
+    message0 <- c(
+      "Can't stratify provided {.arg pop_data}
+       with the provided {.arg strata}:",
+      "i" = "variable {.var {missing_strata_vars}}
+             {?is/are} missing in {.arg pop_data}."
+    )
 
-    partial_matches =
-      purrr::map(missing_strata_vars, function(x)
+    partial_matches <-
+      purrr::map(missing_strata_vars, function(x) {
         stringr::str_subset(string = names(pop_data), pattern = x) %>%
           glue::backtick() %>%
-          and::or()) %>%
+          and::or()
+      }) %>%
       rlang::set_names(missing_strata_vars) %>%
-      purrr::keep( ~ length(.x) > 0)
+      purrr::keep(~ length(.x) > 0)
 
-    inputs_with_partial_matches = names(partial_matches)
+    inputs_with_partial_matches <- names(partial_matches) # nolint: object_usage_linter
 
-    if (length(partial_matches) > 0)
-    {
-      partial_matches =
+    if (length(partial_matches) > 0) {
+      partial_matches <-
         glue::glue("\"{names(partial_matches)}\": {partial_matches}")
 
-      message0 = c(
+      message0 <- c(
         message0,
         "i" = "The following input{?s} to {.arg strata}
                   might be misspelled:
@@ -44,7 +51,6 @@ check_strata <- function(pop_data, strata) {
         "i" = "Did you mean:",
         partial_matches %>% rlang::set_names("*")
       )
-
     }
 
     cli::cli_abort(
@@ -52,7 +58,6 @@ check_strata <- function(pop_data, strata) {
       call = rlang::caller_env(),
       message = message0
     )
-
   }
 
   invisible(NULL)

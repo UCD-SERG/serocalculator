6a5d83482536b0595b8fd8a3df9e48378a6151e6
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-12-03 19:30:01 -0800

    lint

--- tests/testthat/test-ab0.R
+++ tests/testthat/test-ab0.R
@@ -2,7 +2,7 @@ test_that("`ab0()` produces consistent results", {
   params1 <-
     data.frame(
       y0 = 10,
-      y1 = 10 ^ 4,
+      y1 = 10^4,
       t1 = 9.5,
       alpha = 0.01,
       r = 1,
@@ -10,12 +10,12 @@ test_that("`ab0()` produces consistent results", {
     ) |>
     as_curve_params()
 
-  calc1 =
+  calc1 <-
     ab0(curve_params = params1, t = 9.4)
 
   expect_snapshot_value(calc1, style = "deparse")
 
-  calc2 =
+  calc2 <-
     ab0(curve_params = params1, t = 9.6)
 
   expect_snapshot_value(calc2, style = "deparse")
@@ -31,14 +31,13 @@ test_that("`ab0()` produces consistent results", {
     ) |>
     as_curve_params()
 
-  calc3 =
+  calc3 <-
     ab0(curve_params = params2, t = 9.4)
 
   expect_snapshot_value(calc3, style = "deparse")
 
-  calc4 =
+  calc4 <-
     ab0(curve_params = params2, t = 9.6)
 
   expect_snapshot_value(calc4, style = "deparse")
-
 })
--- tests/testthat/test-plot_curve_params_one_ab.R
+++ tests/testthat/test-plot_curve_params_one_ab.R
@@ -13,11 +13,11 @@ test_that("`plot_curve_params_one_ab()` produces consistent results", {
 
   fig1 <-
     params |>
-    plot_curve_params_one_ab(n_points = 10^5,
-                             xlim = c(0,25),
+    plot_curve_params_one_ab(n_points = 10 ^ 5,
+                             xlim = c(0, 25),
                              log_y = FALSE)
 
   fig1 |>
-  vdiffr::expect_doppelganger(title = "curve_r1")
+    vdiffr::expect_doppelganger(title = "curve_r1")
 
 })

7b7f23be89c4e51680009b57e046ec5a0ca65f50
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-07 22:14:51 -0800

    lint

--- tests/testthat/test-serocalculator_example.R
+++ tests/testthat/test-serocalculator_example.R
@@ -1,14 +1,14 @@
 test_that("`serocalculator_example()` works", {
-
-  path1 = serocalculator_example("example_pop_data.csv")
-  path2 = fs::path_package(package = "serocalculator",
-                           "extdata/example_pop_data.csv")
+  path1 <- serocalculator_example("example_pop_data.csv")
+  path2 <- fs::path_package(
+    package = "serocalculator",
+    "extdata/example_pop_data.csv"
+  )
   expect_equal(path1, path2)
 
 
-  files1 = serocalculator_example()
-  files2 = "example_pop_data.csv"
+  files1 <- serocalculator_example()
+  files2 <- "example_pop_data.csv"
 
   expect_equal(files1, files2)
-
 })

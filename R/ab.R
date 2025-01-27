7f39726583c0f45ecbd227cf5b7c8d6ea4f70f5d
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-27 14:58:02 -0800

    fixed lint

--- R/ab.R
+++ R/ab.R
@@ -15,14 +15,17 @@ ab <- function(t, par, ...) {
   alpha <- par["alpha", ]
   shape <- par["shape_r", ]
   yt <- array(0, dim = c(length(t), ncol(par)))
-  for (k in 1:ncol(par)) {
+  for (k in seq_len(ncol(par))) {
     u <- (t <= t1[k])
     d <- (t > t1[k])
     yt[u, k] <- y0[k] * exp(mu1[k] * t[u])
     if (shape[k] != 1) {
-      # this is a version of Eq 14 from Teunis et al 2014, factoring in the first y1 term
-      yt[d, k] <- (y1[k]^(1 - shape[k]) -
-        (1 - shape[k]) * alpha[k] * (t[d] - t1[k]))^(1 / (1 - shape[k]))
+      # this is a version of Eq 14 from Teunis et al 2014,
+      # factoring in the first y1 term
+      term1 <- y1[k]^(1 - shape[k])
+      term2 <- (1 - shape[k]) * alpha[k] * (t[d] - t1[k])
+      exponent <- (1 / (1 - shape[k]))
+      yt[d, k] <- (term1 - term2)^exponent
     }
     if (shape[k] == 1) yt[d, k] <- y1[k] * exp(-alpha[k] * t[d])
     yt[, k] <- baseline(k, yt[, k], ...)

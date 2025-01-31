0862d29c10e3024a66a3b6d684313ae4278e8738
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-30 20:14:43 -0800

    in progress

--- data-raw/sample-size.R
+++ data-raw/sample-size.R
@@ -1,22 +1,25 @@
 library(dplyr)
 library(tibble)
 library(serocalculator)
+library(furrr)
 
 ###############################################################################
 ## Load longitudinal parameters
 
-test_sim<-"https://osf.io/download/rtw5k" %>%
+test_sim <- "https://osf.io/download/rtw5k" %>%
   load_curve_params() %>%
   dplyr::filter(iter < 500)
 ###############################################################################
 
 # Define the simulation function
-simulate_seroincidence <- function(nrep, n_sim) {
-  # Parameters
-  dmcmc <- test_sim  # Curve parameters
-  antibodies <-c("HlyE_IgA", "HlyE_IgG")
-  lambda <- 0.2  # Simulated incidence rate per person-year
-  lifespan <- c(0, 10)  # Age range
+simulate_seroincidence <- function(
+    nrep,
+    n_sim,
+    dmcmc = test_sim,  # Curve parameters
+    antibodies = c("HlyE_IgA", "HlyE_IgG"),
+    lambda = 0.2,  # Simulated incidence rate per person-year
+    lifespan = c(0, 10)  # Age range
+    ) {
 
   # biologic noise distribution
   dlims <- rbind(
@@ -33,8 +36,7 @@ simulate_seroincidence <- function(nrep, n_sim) {
     y.high = c(5e6, 5e6)  # High cutoff (y.high)
   )
 
-  # Perform simulations in parallel
-  results <- furrr::future_map(1:n_sim, function(i) {
+  run_one_sim_iter <- function(i) {
     # Generate cross-sectional data
     csdata <- sim.cs(
       curve_params = dmcmc,
@@ -55,7 +57,7 @@ simulate_seroincidence <- function(nrep, n_sim) {
       curve_params = dmcmc,
       noise_params = cond,
       lambda_start = 0.1,
-      build_graph = TRUE,
+      build_graph = FALSE,
       verbose = FALSE,
       print_graph = FALSE,
       antigen_isos = antibodies
@@ -66,7 +68,21 @@ simulate_seroincidence <- function(nrep, n_sim) {
       csdata = csdata,
       est1 = est
     )
-  }, .options = furrr_options(seed = TRUE))
+  }
+
+
+  # Perform simulations in parallel
+  results <-
+    furrr::future_map(
+      .x = 1:n_sim,
+      .f = run_one_sim_iter,
+      .options = furrr_options(seed = TRUE)) |>
+    structure(
+      antibodies = antibodies,
+      lambda = lambda, # Simulated incidence rate per person-year
+      lifespan = lifespan, # Age range
+      sample_size = nrep
+    )
 
   return(results)
 }
@@ -78,22 +94,30 @@ future::plan(multisession)  # Use multiple sessions for parallelism (local machi
 
 # Run the simulations in parallel
 set.seed(129251)
-results_50 <- simulate_seroincidence(nrep = 50, n_sim = 300)
+results_50 <- simulate_seroincidence(lambda = 0.2, nrep = 50, n_sim = 300)
 
 set.seed(129252)
-results_100 <- simulate_seroincidence(nrep = 100, n_sim = 300)
+results_100 <- simulate_seroincidence(lambda = 0.2, nrep = 100, n_sim = 300)
+
+results_100_0.1 <- simulate_seroincidence(lambda = 0.1, nrep = 100, n_sim = 300)
+
+results_100_0.01 <- simulate_seroincidence(lambda = 0.1, nrep = 100, n_sim = 300)
 
 # Stop parallel processing
-plan(sequential)  # Return to sequential processing
+future::plan(sequential)  # Return to sequential processing
 
+n_obs <- function(results_list) {
+  results_list |> attr("sample_size")
+}
 
 # Define a function to generate final tables
-generate_final_table <- function(results_list, sample_size) {
+generate_final_table <- function(results_list,
+                                 sample_size = n_obs(results_list)) {
   # Initialize an empty list to store the results
   summary_results <- list()
 
   # Loop through each of the 100 results and extract the required columns
-  for (i in 1:300) {
+  for (i in 1:length(results_list)) {
     # Extract the summary for each result
     result_summary <- summary(results_list[[i]]$est1)
 
@@ -113,26 +137,46 @@ generate_final_table <- function(results_list, sample_size) {
   final_table <- bind_rows(summary_results) %>%
     mutate(sample_size = sample_size) # Add sample size column for clarity
 
+  attributes(final_table) <-
+    c(
+      attributes(final_table),
+      attributes(results_list)
+    )
+
   return(final_table)
 }
 
 # Store each sample size's summary as table
-final_table_50 <- generate_final_table(results_50, 50)
-final_table_100 <- generate_final_table(results_100, 100)
+final_table_50 <- results_50 |> generate_final_table(sample_size = 50)
+final_table_100 <- results_100 |> generate_final_table(sample_size = 100)
+final_table_100_0.1 <- results_100_0.1 |> generate_final_table(sample_size = 100)
+ft_100_0.01 <- results_100_0.01 |> generate_final_table()
+lambda <- function(results_table)
+{
+  results_table |> attr("lambda")
+}
 
+compute_coverage <- function(
+    results_table,
+    true_lambda = results_table |> lambda()) {
 
-# Define the true lambda
-true_lambda <- 0.2
+  results_table %>%
+    dplyr::mutate(
+      covered = CI.lwr <= true_lambda & CI.upr >= true_lambda,
+      no_error = !is.na(SE)) %>%
+    dplyr::summarize(
+      no_error_count = sum(no_error),
+      coverage_count = sum(covered, na.rm = TRUE),
+      coverage_rate = mean(covered, na.rm = TRUE))
+}
 
 # Check how many rows have CI covering the true lambda
-coverage_count_50 <- final_table_50 %>%
-  filter(CI.lwr <= true_lambda & CI.upr >= true_lambda) %>%
-  nrow()
+coverage_count_50 <- final_table_50 %>% compute_coverage() |> print()
 
-coverage_count_100 <- final_table_100 %>%
-  filter(CI.lwr <= true_lambda & CI.upr >= true_lambda) %>%
-  nrow()
+coverage_count_100 <- final_table_100 %>% compute_coverage() |> print()
 
+final_table_100_0.1 |> compute_coverage()
+ft_100_0.01 |> compute_coverage()
 # Print the result
 print(paste("Number of rows where CI covers true lambda:", coverage_count_50))
 print(paste("Number of rows where CI covers true lambda:", coverage_count_100))

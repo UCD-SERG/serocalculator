dd1e3cd331c8d4ee6c748a99d56bc5250ef12605
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-03 14:12:08 -0800

    not sure what went wrong

--- tests/testthat/test-graph.curve.params.R
+++ tests/testthat/test-graph.curve.params.R
@@ -1,13 +1,13 @@
-test_that("results are consistent", {
-   curve <-
-     typhoid_curves_nostrat_100 |>
-     dplyr::filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG"))
-
-   plot1 <- graph.curve.params(curve)
-
-   plot1 |> vdiffr::expect_doppelganger(title = "curve-quantiles")
-
-   plot2 <- graph.curve.params(curve, show_all_curves = TRUE)
-
-   plot2 |> vdiffr::expect_doppelganger(title = "curve-quantiles-and-samples")
-})
+test_that(
+  desc = "results are consistent",
+  code = {
+    curve <-
+      typhoid_curves_nostrat_100 |>
+      dplyr::filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG"))
+    plot1 <- graph.curve.params(curve)
+    plot1 |> vdiffr::expect_doppelganger(title = "curve-quantiles")
+    plot2 <- graph.curve.params(curve, show_all_curves = TRUE)
+    plot2 |>
+      vdiffr::expect_doppelganger(title = "curve-quantiles-and-samples")
+  }
+)

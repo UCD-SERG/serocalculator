dd34de6f3c88e9ea05fdcf8cae881ad65fa02610
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-06 16:19:15 -0800

    more

--- R/simpar.R
+++ R/simpar.R
@@ -1,4 +1,9 @@
 # age doesn't seem to be used?
+# this function converts the
+# 4 parameter + y0 format ("5 parameters + initial conditions)
+# into the 5 parameters + y0 + y1 + t1 format ("3 parameters + initial conditions")
+# it assumes that b0 = 1, and seems to derive one
+# extra degree of freedom from the linked differential equations
 simpar <- function(age, k.test, nmc, ...) {
   y0.mc <- par.pred.n(1, k.test, nmc, ...)
   b0.mc <- 1

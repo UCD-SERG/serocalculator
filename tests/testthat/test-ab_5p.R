60847842c68996cc819323086d9cd6b95e819755
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-13 17:14:59 -0800

    tests

--- tests/testthat/test-ab_5p.R
+++ tests/testthat/test-ab_5p.R
@@ -1,11 +1,26 @@
 test_that("results are consistent without units", {
   params <- typhoid_curves_nostrat_100[1, ]
-  ab_5p(
-    t = 50,
-    y0 = params$y0,
-    y1 = params$y1,
-    t1 = params$t1,
-    alpha = params$alpha,
-    shape = params$r) |>
+  results1 <- tibble::tibble(
+    t = seq(0, 100, by = 5),
+    y = ab_5p(
+      t = t,
+      y0 = params$y0,
+      y1 = params$y1,
+      t1 = params$t1,
+      alpha = params$alpha,
+      shape = params$r)
+  )
+
+  results1 |>
     expect_snapshot_value(style = "deparse")
+
+  fig1 <-
+    results1 |>
+    ggplot2::ggplot() +
+    ggplot2::aes(x = t, y = y) +
+    ggplot2::geom_point() +
+    ggplot2::geom_line()
+
+  fig1 |> vdiffr::expect_doppelganger(title = "response_curve")
+
 })

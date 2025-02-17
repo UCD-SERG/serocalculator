3fc11ed151c4956d03c87685895f2e8a53510e28
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-17 15:18:19 -0800

    decomposed `get_biomarker_names()`, added test

--- R/class_attributes.R
+++ R/class_attributes.R
@@ -78,15 +78,6 @@ get_biomarker_levels <- function(object, ...) {
   }
 }
 
-get_biomarker_names <- function(object, ...) {
-  # get biomarker name data
-  biomarker_names_var <- get_biomarker_names_var(object)
-  biomarker_data <- object |> pull(biomarker_names_var)
-
-  return(biomarker_data)
-}
-
-
 set_age <- function(object,
                     age = "Age",
                     standardize = TRUE,

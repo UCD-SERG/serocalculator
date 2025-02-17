e0b79c76f8e5f759a4f1b7a6c5463a1a4bb7c4ac
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-17 15:21:19 -0800

    add `get_biomarker_names()` to API

--- R/get_biomarker_names.R
+++ R/get_biomarker_names.R
@@ -1,3 +1,14 @@
+#' Extract biomarker names column
+#'
+#' @param object
+#' a long-format dataset (`pop_data`, `curve_params`, etc)
+#' @param ... unused
+#'
+#' @returns a [character] or [factor] [vector] of biomarker names
+#' @export
+#'
+#' @examples
+#' sees_pop_data_100 |> get_biomarker_names() |> head()
 get_biomarker_names <- function(object, ...) {
   # get biomarker name data
   biomarker_names_var <- get_biomarker_names_var(object)

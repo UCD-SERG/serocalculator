9acc9688b966f46671e4fde1e20edcb7bd24ed2b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-18 17:43:35 -0800

    lint

--- R/sim_time_since_last_sc.R
+++ R/sim_time_since_last_sc.R
@@ -10,7 +10,9 @@ sim_time_since_last_sc <- function(lambda, n_samples, age) {
     t <- t |>
       units::as_units(t_units)
     extra_val <- units::as_units(Inf, t_units)
-  } else extra_val <- Inf
+  } else {
+    extra_val <- Inf
+  }
 
   t <- if_else(t > age, extra_val, t)
 

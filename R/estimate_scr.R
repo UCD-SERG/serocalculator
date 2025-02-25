beb08908b3c0fc310b6885f286032482966ebb20
kristinawlai <kwlai@ucdavis.edu>
2025-02-25 13:20:01 -0800

    deprecate est.incidence

--- R/estimate_scr.R
+++ R/estimate_scr.R
@@ -212,3 +212,20 @@ estimate_scr <- function(
 
   return(fit)
 }
+
+#' @title Estimate Seroincidence
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `est.incidence()` was renamed to [estimate_scr()] to create a more
+#' consistent API.
+#' @keywords internal
+#' @export
+est.incidence <- function(
+    ...) {
+  lifecycle::deprecate_soft("1.3.1", "est.incidence()", "estimate_scr()")
+  estimate_scr(
+    ...
+  )
+}

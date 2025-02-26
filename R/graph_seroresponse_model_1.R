8d796983054c1db348525d999466d2ab923c55c2
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-02-25 16:29:04 -0800

    in progress

--- R/graph_seroresponse_model_1.R
+++ R/graph_seroresponse_model_1.R
@@ -10,14 +10,17 @@
 #' the row numbers are enumerated separately for each antigen isotype;
 #' in other words, for the purposes of this argument,
 #' row numbers start over at 1 for each antigen isotype.
-#' There is currently no way to specify different row numbers for different antigen isotypes;
+#' There is currently no way to specify different row numbers
+#' for different antigen isotypes;
 #' if you want to do that,
-#' you will could call [plot_curve_params_one_ab()] directly for each antigen isotype
+#' you will could call [plot_curve_params_one_ab()] directly
+#' for each antigen isotype
 #' and combine the resulting panels yourself.
 #' Or you could subset `curve_params` manually,
 #' before passing it to this function,
 #' and set the `n_curves` argument to `Inf`.
 #' @return a [ggplot2::ggplot()] object
+#' @keywords internal
 #' @export
 #' @examples
 #' \donttest{
@@ -26,24 +29,44 @@
 #' library(magrittr)
 #'
 #' curve <-
-#'   serocalculator_example("example_curve_params.csv") %>%
-#'   read.csv() %>%
-#'   as_curve_params() %>%
-#'   filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG")) %>%
-#'   autoplot()
+#'   serocalculator_example("example_curve_params.csv") |>
+#'   read.csv() |>
+#'   as_curve_params() |>
+#'   filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG")) |>
+#'   graph_seroresponse_model_1()
 #'
 #' curve
 #' }
-autoplot.curve_params <- function(
+graph_seroresponse_model_1 <- function(
     object,
     antigen_isos = unique(object$antigen_iso),
     ncol = min(3, length(antigen_isos)),
     ...) {
+  split_data <- object |>
+    filter(.data$antigen_iso %in% antigen_isos) |>
+    droplevels() |>
+    split(~antigen_iso)
 
-  object |>
-    graph_seroresponse_model_1(
-      antigen_isos = antigen_isos,
-      ncol = ncol,
-      ...
+  labels <- names(split_data)
+  figs <- split_data |>
+    lapply(FUN = plot_curve_params_one_ab, ...)
+
+  for (i in seq_along(figs)) {
+    figs[[i]] <- figs[[i]] + ggplot2::ggtitle(labels[i])
+  }
+
+
+  nrow <- ceiling(length(figs) / ncol)
+  figure <- do.call(
+    what = function(...) {
+      ggpubr::ggarrange(
+        ...,
+        ncol = ncol,
+        nrow = nrow
+      )
+    },
+    args = figs
   )
+
+  return(figure)
 }

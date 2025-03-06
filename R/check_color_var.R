a46c4ff60cdcd2c6737d9b8277689b17fdde3830
kristinawlai <kwlai@ucdavis.edu>
2025-03-06 10:44:20 -0800

    Update vignette with "type" argument

---
+++ R/check_color_var.R
@@ -0,0 +1,16 @@
+check_color_var <- function(
+    color_var,
+    objects
+) {
+
+  # Check if color_var exists
+  if (!is.null(color_var) && !is.element(color_var, names(object))) {
+    cli::cli_abort(
+      class = "unavailable_color_var",
+      message = c(
+        "The variable `{color_var}` specified by argument `color_var` does not exist in `object`.",
+        "Please choose a column that exists in `object`."
+      )
+    )
+  }
+}

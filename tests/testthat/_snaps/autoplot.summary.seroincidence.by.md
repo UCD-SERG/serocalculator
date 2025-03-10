1715707bfcf80969d26fbc166ac1a6909b03447d
kristinawlai <kwlai@ucdavis.edu>
2025-03-10 10:33:08 -0700

    Add tests

--- tests/testthat/_snaps/autoplot.summary.seroincidence.by.md
+++ tests/testthat/_snaps/autoplot.summary.seroincidence.by.md
@@ -19,3 +19,24 @@
       ! Invalid plot `type` specified: "whisker".
       i Please choose either 'scatter' or 'bar'.
 
+# The variable `{yvar}` specified by argument `yvar` does not exist in `object`. Please choose a column that exists in `object`.
+
+    Code
+      library(dplyr)
+      library(ggplot2)
+      xs_data <- sees_pop_data_pk_100
+      curve <- filter(typhoid_curves_nostrat_100, antigen_iso %in% c("HlyE_IgA",
+        "HlyE_IgG"))
+      noise <- example_noise_params_pk
+      est2 <- estimate_scr_by(strata = c("catchment", "ageCat"), pop_data = xs_data,
+      curve_params = curve, noise_params = noise, curve_strata_varnames = NULL,
+      noise_strata_varnames = NULL, antigen_isos = c("HlyE_IgG", "HlyE_IgA"),
+      num_cores = 2)
+      est2sum <- summary(est2)
+      plot1 <- autoplot(est2sum, yvar = "fake", type = "bar", dodge_width = 0.1,
+        color_var = "catchment", CI = TRUE)
+    Condition
+      Error in `strat_ests_barplot()`:
+      ! The variable `fake` specified by argument `yvar` does not exist in `object`.
+      Please choose a column that exists in `object`.
+
--- tests/testthat/test-autoplot.summary.seroincidence.by.R
+++ tests/testthat/test-autoplot.summary.seroincidence.by.R
@@ -142,4 +142,42 @@ test_that("Invalid plot `type` specified. Please choose either 'scatter' or 'bar
   })
 )
 
+test_that("The variable `{yvar}` specified by argument `yvar` does not exist in `object`. Please choose a column that exists in `object`.",
+          expect_snapshot(error = TRUE, {
 
+            library(dplyr)
+            library(ggplot2)
+
+            xs_data <-
+              sees_pop_data_pk_100
+
+            curve <-
+              typhoid_curves_nostrat_100 |>
+              filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG"))
+
+            noise <-
+              example_noise_params_pk
+
+            est2 <- estimate_scr_by(
+              strata = c("catchment", "ageCat"),
+              pop_data = xs_data,
+              curve_params = curve,
+              noise_params = noise,
+              curve_strata_varnames = NULL,
+              noise_strata_varnames = NULL,
+              antigen_isos = c("HlyE_IgG", "HlyE_IgA"),
+              num_cores = 2 # Allow for parallel processing to decrease run time
+            )
+
+            est2sum <- summary(est2)
+
+            plot1 <- autoplot(est2sum,
+                              yvar = "fake",
+                              type = "bar",
+                              dodge_width = 0.1,
+                              color_var = "catchment",
+                              CI = TRUE)
+
+          }
+          )
+)

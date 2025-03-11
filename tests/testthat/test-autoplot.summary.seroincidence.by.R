9b4633bd2498f11c6026993f0edb91a36b95d335
kristinawlai <kwlai@ucdavis.edu>
2025-03-11 12:19:39 -0700

    Update test syntax and add additional tests

--- tests/testthat/test-autoplot.summary.seroincidence.by.R
+++ tests/testthat/test-autoplot.summary.seroincidence.by.R
@@ -105,8 +105,10 @@ test_that(
   }
 )
 
-test_that("Invalid plot `type` specified. Please choose either 'scatter' or 'bar'.",
-  expect_snapshot(error = TRUE, {
+test_that(
+  desc = "Invalid plot `type` specified. Please choose either 'scatter' or 'bar'.",
+  code = {
+
     library(dplyr)
     library(ggplot2)
 
@@ -133,56 +135,58 @@ test_that("Invalid plot `type` specified. Please choose either 'scatter' or 'bar
 
     est2sum <- summary(est2)
 
- expect_snapshot(
-        error = TRUE, 
-        x = {
-             plot1 <- autoplot(est2sum,
-                      xvar = "ageCat",
-                      type = "whisker",
-                      dodge_width = 0.1,
-                      color_var = "catchment",
-                      CI = TRUE)
-          }
+    expect_snapshot(
+      error = TRUE,
+      x = {
+        plot1 <- autoplot(est2sum,
+                          xvar = "ageCat",
+                          type = "whisker", #invalid plot type
+                          dodge_width = 0.1,
+                          color_var = "catchment",
+                          CI = TRUE)
+      }
     )
   })
-)
 
-test_that("The variable `{yvar}` specified by argument `yvar` does not exist in `object`. Please choose a column that exists in `object`.",
-          expect_snapshot(error = TRUE, {
+test_that(
+  desc = "The variable `{yvar}` specified by argument `yvar` does not exist in `object`. Please choose a column that exists in `object`.",
+  code = {
+    library(dplyr)
+    library(ggplot2)
 
-            library(dplyr)
-            library(ggplot2)
+    xs_data <-
+      sees_pop_data_pk_100
 
-            xs_data <-
-              sees_pop_data_pk_100
+    curve <-
+      typhoid_curves_nostrat_100 |>
+      filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG"))
 
-            curve <-
-              typhoid_curves_nostrat_100 |>
-              filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG"))
+    noise <-
+      example_noise_params_pk
 
-            noise <-
-              example_noise_params_pk
+    est2 <- estimate_scr_by(
+      strata = c("catchment", "ageCat"),
+      pop_data = xs_data,
+      curve_params = curve,
+      noise_params = noise,
+      curve_strata_varnames = NULL,
+      noise_strata_varnames = NULL,
+      antigen_isos = c("HlyE_IgG", "HlyE_IgA"),
+      num_cores = 2 # Allow for parallel processing to decrease run time
+    )
 
-            est2 <- estimate_scr_by(
-              strata = c("catchment", "ageCat"),
-              pop_data = xs_data,
-              curve_params = curve,
-              noise_params = noise,
-              curve_strata_varnames = NULL,
-              noise_strata_varnames = NULL,
-              antigen_isos = c("HlyE_IgG", "HlyE_IgA"),
-              num_cores = 2 # Allow for parallel processing to decrease run time
-            )
+    est2sum <- summary(est2)
 
-            est2sum <- summary(est2)
+    expect_snapshot(
+      error = TRUE,
+      x = {
+        plot1 <- autoplot(est2sum,
+                          yvar = "fake", #invalid variable name
+                          type = "bar",
+                          dodge_width = 0.1,
+                          color_var = "catchment",
+                          CI = TRUE)
 
-            plot1 <- autoplot(est2sum,
-                              yvar = "fake",
-                              type = "bar",
-                              dodge_width = 0.1,
-                              color_var = "catchment",
-                              CI = TRUE)
-
-          }
-          )
+      }
+    )}
 )

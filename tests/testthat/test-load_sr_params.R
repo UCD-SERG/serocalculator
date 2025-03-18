79101f9fcc5b1151dde8e6ef735798f1fd52b18d
kristinawlai <kwlai@ucdavis.edu>
2025-03-17 17:22:12 -0700

    Fix lints and add test

---
+++ tests/testthat/test-load_sr_params.R
@@ -0,0 +1,23 @@
+test_that(
+  desc = "`load_sr_params()` produces expected results",
+  code = {
+    expect_no_error(
+      sr_params_true <-
+        load_sr_params(
+          serocalculator_example("example_curve_params.rds")
+        )
+    )
+  }
+)
+
+test_that(
+  desc = "`load_sr_params()` produces error with non-curve data",
+  code = {
+    expect_error(
+      sr_params_true <-
+        load_sr_params(
+          serocalculator_example("example_pop_data.rds")
+        )
+    )
+  }
+)

10e8a18fc99f18427e6ad9954f1a0c4dca1bd938
kristinawlai <kwlai@ucdavis.edu>
2025-03-17 21:53:26 -0700

    deprecate old function

--- R/load_sr_params.R
+++ R/load_sr_params.R
@@ -1,7 +1,7 @@
-#' Load antibody decay curve parameter samples
+#' Load longitudinal seroresponse parameter samples
 #'
 #' @param file_path path to an RDS file containing MCMC samples of antibody
-#' decay curve parameters `y0`, `y1`, `t1`, `alpha`, and `r`,
+#' seroresponse parameters `y0`, `y1`, `t1`, `alpha`, and `r`,
 #' stored as a [data.frame()] or [tibble::tbl_df]
 #' @param antigen_isos [character()] vector of antigen isotypes used in analyses
 #'
@@ -24,4 +24,21 @@ load_sr_params <- function(file_path, antigen_isos = NULL) {
     as_curve_params()
 
   return(curve_params)
+}
+
+#' @title Load antibody decay curve parameter samples
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `load_curve_params()` was renamed to [load_sr_params()] to create a more
+#' consistent API.
+#' @keywords internal
+#' @export
+load_curve_params <- function(
+    ...) {
+  lifecycle::deprecate_soft("1.3.1", "load_curve_params()", "load_sr_params()")
+  load_sr_params(
+    ...
+  )
 }

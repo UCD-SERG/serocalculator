a3045bcf3deb1571fd65522378cd5af55f4a9d89
unknown <ksjlee@ucdavis.edu>
2025-04-24 14:43:33 -0700

    # Refactor graph.curve.params(): numeric quantiles, remove show_quantiles

--- tests/testthat/test-graph.curve.params.R
+++ tests/testthat/test-graph.curve.params.R
@@ -1,28 +1,26 @@
 test_that(
   desc = "results are consistent",
   code = {
-    curve <-
-      typhoid_curves_nostrat_100 |>
+    curve <- typhoid_curves_nostrat_100 |>
       dplyr::filter(antigen_iso %in% c("HlyE_IgA", "HlyE_IgG")) |>
       dplyr::mutate(.by = antigen_iso, chain = rep(1:2, times = n() / 2))
+
+    # 1. Default quantiles: c(0.1, 0.5, 0.9)
     plot1 <- graph.curve.params(curve)
-    plot1 |> vdiffr::expect_doppelganger(title = "curve-quantiles")
+    vdiffr::expect_doppelganger(title = "curve-quantiles", fig = plot1)
+
+    # 2. Default quantiles + all MCMC samples
     plot2 <- graph.curve.params(curve, show_all_curves = TRUE)
-    plot2 |>
-      vdiffr::expect_doppelganger(title = "curve-quantiles-and-samples")
+    vdiffr::expect_doppelganger(title = "curve-quantiles-and-samples",
+                                fig = plot2)
+
+    # 3. Custom numeric quantiles only
     plot3 <- graph.curve.params(
       curve,
       show_all_curves = TRUE,
-      show_quantiles = FALSE
+      quantiles = c(0.05, 0.55, 0.95)
     )
-    plot3 |>
-      vdiffr::expect_doppelganger(title = "curve-samples")
-    # New test: custom quantiles only
-    plot4 <- graph.curve.params(
-      curve,
-      show_quantiles = TRUE,
-      quantiles = c("med")
-    )
-    plot4 |> vdiffr::expect_doppelganger(title = "curve-custom-quantiles")
+    vdiffr::expect_doppelganger(title = "curve-custom-quantiles", fig = plot3)
   }
 )
+

24078f1ff8cc00ae5da7f592c170f07d612ad645
kristinawlai <kwlai@ucdavis.edu>
2025-06-23 17:07:54 -0700

    Add more tests

--- tests/testthat/test-autoplot.seroincidence.by.R
+++ tests/testthat/test-autoplot.seroincidence.by.R
@@ -15,3 +15,19 @@ test_that("Build graphs not TRUE", {
    expect_error()
 })
 
+test_that("Build graphs works as expected", {
+
+  est2 <- est_seroincidence_by(
+    strata = c("catchment"),
+    pop_data = sees_pop_data_pk_100,
+    curve_params = typhoid_curves_nostrat_100,
+    curve_strata_varnames= NULL,
+    noise_strata_varnames = NULL,
+    noise_params = example_noise_params_pk,
+    antigen_isos = c("HlyE_IgG", "HlyE_IgA"),
+    build_graph = TRUE
+  ) |>
+    autoplot() |>
+    vdiffr::expect_doppelganger(title = "seroinc-plot")
+})
+

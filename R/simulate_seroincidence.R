b86f14b56353da8e2f0fe95193b4ed80ada8be76
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-06-26 21:27:26 -0700

    more cleanup

--- R/simulate_seroincidence.R
+++ R/simulate_seroincidence.R
@@ -1,35 +1,49 @@
 #' Simulate seroincidence
 #'
-#' @param nrep
-#' @param n_sim
-#' @param renew.params
+#' @param nrep number of samples per simulated dataset
+#' @param n_sim number of simulations to run
+#' @param renew.params whether to sample new parameters for each infection
+#' in a simulated individual's longitudinal data
+#' @param sr_params Curve parameters
+#' @param antibodies which antibodies to simulate
+#' @param lambda Simulated incidence rate per person-year
+#' @param lifespan range of ages to simulate
+#' @param dlims biologic noise distribution
+#' @param cond Noise parameters
 #'
-#' @returns
+#' @returns a [list] of simulation results
 #' @export
 #' @keywords internal
 #'
 #' @examples
-simulate_seroincidence <- function(nrep, n_sim, renew.params = TRUE) {
+#' simulate_seroincidence(sr_params = typhoid_curves_nostrat_100)
+simulate_seroincidence <- function(
+    nrep = 100,
+    n_sim = 10,
+    sr_params,
+    renew.params = TRUE,
+    antibodies = c("HlyE_IgA", "HlyE_IgG"),
+    lambda = 0.01,
+    lifespan = c(0, 10),  # Age range
+
+    #
+    dlims = rbind(
+      "HlyE_IgA" = c(min = 0, max = 0.5),
+      "HlyE_IgG" = c(min = 0, max = 0.5)
+    ),
+
+    #
+    cond = tibble(
+      antigen_iso = c("HlyE_IgG", "HlyE_IgA"),
+      nu = c(0.5, 0.5),  # Biologic noise (nu)
+      eps = c(0, 0),     # Measurement noise (eps)
+      y.low = c(1, 1),   # Low cutoff (llod)
+      y.high = c(5e6, 5e6)  # High cutoff (y.high)
+    )
+) {
   # Parameters
-  dmcmc <- test_sim  # Curve parameters
-  antibodies <-c("HlyE_IgA", "HlyE_IgG")
-  lambda <- 0.01  # Simulated incidence rate per person-year
-  lifespan <- c(0, 10)  # Age range
+  dmcmc <- sr_params  #
 
-  # biologic noise distribution
-  dlims <- rbind(
-    "HlyE_IgA" = c(min = 0, max = 0.5),
-    "HlyE_IgG" = c(min = 0, max = 0.5)
-  )
-
-  # Noise parameters
-  cond <- tibble(
-    antigen_iso = c("HlyE_IgG", "HlyE_IgA"),
-    nu = c(0.5, 0.5),  # Biologic noise (nu)
-    eps = c(0, 0),     # Measurement noise (eps)
-    y.low = c(1, 1),   # Low cutoff (llod)
-    y.high = c(5e6, 5e6)  # High cutoff (y.high)
-  )
 
   # Perform simulations in parallel
   results <- future_map(1:n_sim, function(i) {
@@ -48,9 +62,9 @@ simulate_seroincidence <- function(nrep, n_sim, renew.params = TRUE) {
     )
 
     # Estimate seroincidence
-    est <- est.incidence(
+    est <- est_seroincidence(
       pop_data = csdata,
-      curve_params = dmcmc,
+      sr_params = dmcmc,
       noise_params = cond,
       lambda_start = 0.005,
       build_graph = TRUE,

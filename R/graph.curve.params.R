7435d62770bc4956ae67e9caf76a87e431289f3d
Kwan Ho Lee <ksjlee@ucdavis.edu>
2025-07-08 10:19:38 -0700

    Add chain_color option to toggle MCMC line coloring by chain or black.

--- R/graph.curve.params.R
+++ R/graph.curve.params.R
@@ -14,6 +14,8 @@
 #' or linear scale (`FALSE`, default)?
 #' @param log_y should the Y-axis be on a logarithmic scale
 #' (default, `TRUE`) or linear scale (`FALSE`)?
+#' @param chain_color logical, if `TRUE` (default), MCMC chain lines are colored by chain.
+#' If `FALSE`, all MCMC chain lines are black.
 #' @inheritParams plot_curve_params_one_ab
 #' @param ... not currently used
 #' @returns a [ggplot2::ggplot()] object showing the antibody dynamic
@@ -31,6 +33,7 @@ graph.curve.params <- function( # nolint: object_name_linter
   verbose = FALSE,
   quantiles = c(0.1, 0.5, 0.9),
   alpha_samples = 0.3,
+  chain_color = TRUE,
   log_x = FALSE,
   log_y = TRUE,
   n_curves = 100,
@@ -151,16 +154,31 @@ graph.curve.params <- function( # nolint: object_name_linter
           group = interaction(across(all_of(group_vars)))
         )
 
-      plot1 <-
-        plot1 +
-        geom_line(
-          data = sc_to_graph,
-          alpha = alpha_samples,
-          aes(
-            color = .data$chain |> factor(),
-            group = .data$group
+      if(chain_color)
+      {
+        plot1 <-
+          plot1 +
+          geom_line(
+            data = sc_to_graph,
+            alpha = alpha_samples,
+            aes(
+              color = .data$chain |> factor(),
+              group = .data$group
+            )
           )
-        )
+      } else {
+        plot1 <-
+          plot1 +
+          geom_line(
+            data = sc_to_graph,
+            alpha = alpha_samples,
+            color = "black",
+            aes(
+              group = .data$group
+            )
+          )
+      }
+
     } else {
       plot1 <-
         plot1 +
@@ -203,7 +221,9 @@ graph.curve.params <- function( # nolint: object_name_linter
 
     if (length(iters_to_graph) > 0) {
       plot1 <- plot1 +
-        ggplot2::labs(col = "MCMC chain")
+        ggplot2::labs(col = "")
+    } else {
+      plot1 <- plot1 + ggplot2::labs(col = "Quantile")
     }
   }
 
